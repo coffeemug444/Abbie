077a12f5c671b07d63fd571d544cd4931946b4ba
Ben Slattery <ben.slattery@taitcommunications.com>
2022-09-07 13:54:36 +1200

    formatting

--- abbie.cpp
+++ abbie.cpp
@@ -5,108 +5,128 @@
 
 using namespace std::chrono_literals;
 using std::chrono::system_clock,
-      std::this_thread::sleep_for,
-      std::cin, std::cout,
-      std::string,
-      std::shared_ptr, std::make_shared, std::make_unique,
-      std::vector,
-      std::future;
+    std::this_thread::sleep_for,
+    std::cin, std::cout,
+    std::string,
+    std::shared_ptr, std::make_shared, std::make_unique,
+    std::vector,
+    std::future;
 
-Abbie::Abbie() {
+Abbie::Abbie()
+{
    rng_ = std::mt19937(dev_());
-   model_ = BenBrain({INPUT_SIZE,2048,2048,1}, 'h');
+   model_ = BenBrain({INPUT_SIZE, 2048, 2048, 1}, 'h');
 }
 
-Abbie::Abbie(std::string modelPath) {
+Abbie::Abbie(std::string modelPath)
+{
    rng_ = std::mt19937(dev_());
    model_ = BenBrain(modelPath);
 }
 
-void Abbie::saveModel(std::string savePath) {
+void Abbie::saveModel(std::string savePath)
+{
    model_.save(savePath);
 }
 
-
-float Abbie::evaluateFEN(string FEN, BenBrain* model) {
+float Abbie::evaluateFEN(string FEN, BenBrain *model)
+{
    Mat input = modelInputFromFEN(FEN);
    Mat output = model->compute(input, BenBrain::leaky_reLU_act);
-   float outval = output.getVal(0,0);
+   float outval = output.getVal(0, 0);
    return outval;
 }
 
-Mat Abbie::modelInputFromFEN(string FEN) {
+Mat Abbie::modelInputFromFEN(string FEN)
+{
    string FEN_pieces = FEN.substr(0, FEN.find(' '));
-   vector<string> rows {};
-   for (int i = 0; i < 6; i++) {
+   vector<string> rows{};
+   for (int i = 0; i < 6; i++)
+   {
       rows.push_back(FEN_pieces.substr(0, FEN_pieces.find('/')));
       FEN_pieces = FEN_pieces.substr(FEN_pieces.find('/') + 1, FEN_pieces.size() - 1);
    }
    rows.push_back(FEN_pieces);
    char squares[NUM_SQUARES];
    unsigned square = 0;
-   for (auto row: rows) {
-      for (auto chr: row) {
-         if (chr >= '1' && chr <= '8') {
+   for (auto row : rows)
+   {
+      for (auto chr : row)
+      {
+         if (chr >= '1' && chr <= '8')
+         {
             unsigned num_empty = chr - '0';
-            for (unsigned i = 0; i < num_empty; i++) {
+            for (unsigned i = 0; i < num_empty; i++)
+            {
                squares[square] = '-';
                square++;
             }
-         } else {
+         }
+         else
+         {
             squares[square] = chr;
             square++;
          }
       }
    }
-   
+
    shared_ptr<float[]> model_input = make_unique<float[]>(INPUT_SIZE);
    vector<char> pieces = {
-      'p', 'P',   // pawn
-      'r', 'R',   // rook
-      'n', 'N',   // knight
-      'b', 'B',   // bishop
-      'q', 'Q',   // queen
-      'k', 'K'    // king
+       'p', 'P', // pawn
+       'r', 'R', // rook
+       'n', 'N', // knight
+       'b', 'B', // bishop
+       'q', 'Q', // queen
+       'k', 'K'  // king
    };
-   
-   for(unsigned pc = 0; pc < NUM_PIECES; pc++) {
-      for (unsigned sq = 0; sq < NUM_SQUARES; sq++) {
+
+   for (unsigned pc = 0; pc < NUM_PIECES; pc++)
+   {
+      for (unsigned sq = 0; sq < NUM_SQUARES; sq++)
+      {
          float val = (pieces[pc] == squares[sq]) ? 1.f : 0.f;
-         model_input[pc*NUM_SQUARES + sq] = val;
+         model_input[pc * NUM_SQUARES + sq] = val;
       }
    }
    return Mat(INPUT_SIZE, 1, model_input);
 }
 
-float std_dev(float* input, unsigned len) {
+float std_dev(float *input, unsigned len)
+{
    float sum = 0;
-   for (unsigned i = 0; i < len; i++) {
+   for (unsigned i = 0; i < len; i++)
+   {
       sum += input[i];
    }
-   float m =  sum / len;
+   float m = sum / len;
 
    float accum = 0.0;
-   for (unsigned i = 0; i < len; i++) {
+   for (unsigned i = 0; i < len; i++)
+   {
       accum += (input[i] - m) * (input[i] - m);
    }
 
-   return sqrt(accum / (len-1)) + 0.000001; // add small constant to std_dev in case all values are exactly the same
+   return sqrt(accum / (len - 1)) + 0.000001; // add small constant to std_dev in case all values are exactly the same
 }
 
-
-void Abbie::evaluateFutureMove(string FEN, Move move, BenBrain* model, float* output) {
-      Board futureBoard(FEN);
-      futureBoard.doMove(move);
-      string futureFEN = futureBoard.genFEN();
-      float futureEval = evaluateFEN(futureFEN, model);
-      assert(!std::isnan(futureEval));
-      *output = futureEval;
+void Abbie::evaluateFutureMove(string FEN, Move move, BenBrain *model, float *output)
+{
+   Board futureBoard(FEN);
+   futureBoard.doMove(move);
+   string futureFEN = futureBoard.genFEN();
+   float futureEval = evaluateFEN(futureFEN, model);
+   assert(!std::isnan(futureEval));
+   *output = futureEval;
 }
 
-void shrinkActivePoolToSize(vector<future<void>>& activePool, unsigned n) {
-   while (activePool.size() >= 6) {
-      for (int i = 0; i < activePool.size(); i++) {
-         if(activePool[i].wait_for(0ms) == std::future_status::ready) {
+void shrinkActivePoolToSize(vector<future<void>> &activePool, unsigned n)
+{
+   while (activePool.size() >= 6)
+   {
+      for (int i = 0; i < activePool.size(); i++)
+      {
+         if (activePool[i].wait_for(0ms) == std::future_status::ready)
+         {
             activePool.erase(activePool.begin() + i);
             i--;
          }
@@ -114,26 +134,29 @@ void shrinkActivePoolToSize(vector<future<void>>& activePool, unsigned n) {
    }
 }
 
+void Abbie::getEvals(vector<Move> legalMoves, float *evaluations, BenBrain *model, string FEN)
+{
+   vector<future<void>> activePool{};
 
-void Abbie::getEvals(vector<Move> legalMoves, float* evaluations, BenBrain* model, string FEN) {
-   vector<future<void>> activePool {};
-
-   for (unsigned i = 0; i < legalMoves.size(); i++) {
+   for (unsigned i = 0; i < legalMoves.size(); i++)
+   {
       shrinkActivePoolToSize(activePool, 6);
       activePool.push_back(std::async(evaluateFutureMove, FEN, legalMoves[i], model, evaluations + i));
    }
-   for (auto& ftr: activePool) {
+   for (auto &ftr : activePool)
+   {
       ftr.wait();
    }
 }
 
-
-Move Abbie::getBotMove(string FEN, float& eval) {
+Move Abbie::getBotMove(string FEN, float &eval)
+{
    Board currentBoard = Board(FEN);
    vector<Move> legalMoves = currentBoard.getLegalMoves();
    int num_moves = legalMoves.size();
    assert(num_moves != 0);
-   if (num_moves == 1) {
+   if (num_moves == 1)
+   {
       return legalMoves[0];
    }
    float evaluations[num_moves];
@@ -145,42 +168,55 @@ Move Abbie::getBotMove(string FEN, float& eval) {
 
    float best_evaluation = evaluations[0];
 
-   for (unsigned i = 1; i < num_moves; i++) {
-      if (playingAsWhite) {
-         if (evaluations[i] > best_evaluation) {
+   for (unsigned i = 1; i < num_moves; i++)
+   {
+      if (playingAsWhite)
+      {
+         if (evaluations[i] > best_evaluation)
+         {
             best_evaluation = evaluations[i];
          }
-      } else {
-         if (evaluations[i] < best_evaluation) {
+      }
+      else
+      {
+         if (evaluations[i] < best_evaluation)
+         {
             best_evaluation = evaluations[i];
          }
       }
    }
 
    float eval_std_dev = std_dev(evaluations, legalMoves.size());
-   vector<Move> bestMoves {};
-   vector<float> bestEvals {};
-   for (unsigned i = 0; i < num_moves; i++) {
-      if (playingAsWhite) {
-         if ((evaluations[i] + eval_std_dev) > best_evaluation) {
+   vector<Move> bestMoves{};
+   vector<float> bestEvals{};
+   for (unsigned i = 0; i < num_moves; i++)
+   {
+      if (playingAsWhite)
+      {
+         if ((evaluations[i] + eval_std_dev) > best_evaluation)
+         {
             bestMoves.push_back(legalMoves[i]);
             bestEvals.push_back(evaluations[i]);
          }
-      } else {
-         if ((evaluations[i] - eval_std_dev) < best_evaluation) {
+      }
+      else
+      {
+         if ((evaluations[i] - eval_std_dev) < best_evaluation)
+         {
             bestMoves.push_back(legalMoves[i]);
             bestEvals.push_back(evaluations[i]);
          }
       }
    }
 
-   std::uniform_int_distribution<std::mt19937::result_type> dist(0,bestMoves.size() - 1);
+   std::uniform_int_distribution<std::mt19937::result_type> dist(0, bestMoves.size() - 1);
    int index = dist(rng_);
    eval = bestEvals[index];
    return bestMoves[index];
 }
 
-void Abbie::playAgainst() {
+void Abbie::playAgainst()
+{
    Chess game;
    game.printBoard();
    GameState state = ONGOING;
@@ -188,30 +224,38 @@ void Abbie::playAgainst() {
    cout << "\n\033[A";
    cout << "Enter move: ";
 
-   while (state == ONGOING || state == BADMOVE) {
+   while (state == ONGOING || state == BADMOVE)
+   {
       string move;
       cin >> move;
-      if (move == "q" || move == "exit") {
+      if (move == "q" || move == "exit")
+      {
          exit(1);
       }
       state = game.acceptMove(move);
 
-      if (state == BADMOVE) {
+      if (state == BADMOVE)
+      {
          cout << "\33[2k\033[A\r";
          cout << "                                                        \r";
          cout << "Move invalid. Enter move: ";
-      } else {
-         for (int i = 0; i < 10; i++) {
+      }
+      else
+      {
+         for (int i = 0; i < 10; i++)
+         {
             // clear line and move up
             cout << "\33[2k\033[A\r";
          }
          game.printBoard();
-         if (state == ONGOING) {
+         if (state == ONGOING)
+         {
             string FEN = game.getFEN();
             float eval = 0.f;
             Move botMove = getBotMove(FEN, eval);
             state = game.acceptMove(botMove);
-            for (int i = 0; i < 9; i++) {
+            for (int i = 0; i < 9; i++)
+            {
                // clear line and move up
                cout << "\33[2k\033[A\r";
             }
@@ -223,55 +267,60 @@ void Abbie::playAgainst() {
    }
 }
 
-Mat Abbie::modelOutputFromVal(float val) {
+Mat Abbie::modelOutputFromVal(float val)
+{
    shared_ptr<float[]> output = make_unique<float[]>(1);
    output[0] = val;
-   return Mat(1,1,output);
+   return Mat(1, 1, output);
 }
 
 void Abbie::compute_W_B_forState(
-      BenBrain* model, 
-      vector<string>* FENs, 
-      unsigned state, 
-      float starting_eval, 
-      float ending_eval, 
-      vector<Mat>* weightGrads, vector<std::shared_ptr<std::mutex>>* w_mtx,
-      vector<Mat>* biasGrads, vector<std::shared_ptr<std::mutex>>* b_mtx
-   ) {
-   float eval_shouldBe = ((ending_eval-starting_eval)/(*FENs).size())*state + starting_eval;
+    BenBrain *model,
+    vector<string> *FENs,
+    unsigned state,
+    float starting_eval,
+    float ending_eval,
+    vector<Mat> *weightGrads, vector<std::shared_ptr<std::mutex>> *w_mtx,
+    vector<Mat> *biasGrads, vector<std::shared_ptr<std::mutex>> *b_mtx)
+{
+   float eval_shouldBe = ((ending_eval - starting_eval) / (*FENs).size()) * state + starting_eval;
    Mat input = modelInputFromFEN((*FENs)[state]);
    Mat output = modelOutputFromVal(eval_shouldBe);
    vector<Mat> diffs;
    vector<Mat> as;
-   
+
    (*model).backPropagate(input, output, diffs, as, BenBrain::leaky_reLU_act, BenBrain::leaky_reLU_inv);
 
    auto nw = (*model).computeWeights(diffs, as);
-   for (int w = 0; w < (*weightGrads).size(); w++) {
+   for (int w = 0; w < (*weightGrads).size(); w++)
+   {
       (*w_mtx)[w]->lock();
-         auto newWeightGrad = (*weightGrads)[w] + nw[w];
-         (*weightGrads)[w] = newWeightGrad;
+      auto newWeightGrad = (*weightGrads)[w] + nw[w];
+      (*weightGrads)[w] = newWeightGrad;
       (*w_mtx)[w]->unlock();
    }
    auto nb = (*model).computeBiases(diffs);
-   for (int b = 0; b < (*biasGrads).size(); b++) {
+   for (int b = 0; b < (*biasGrads).size(); b++)
+   {
       (*b_mtx)[b]->lock();
-         auto newBiasGrad = (*biasGrads)[b] + nb[b];
-         (*biasGrads)[b] = newBiasGrad;
+      auto newBiasGrad = (*biasGrads)[b] + nb[b];
+      (*biasGrads)[b] = newBiasGrad;
       (*b_mtx)[b]->unlock();
    }
 }
 
-void Abbie::trainOneGame() {
+void Abbie::trainOneGame()
+{
    Chess game;
    GameState gameState = ONGOING;
-   vector<string> FENs {};
+   vector<string> FENs{};
 
    std::cout << "turn 0\n";
 
    bool whitePlaying = true;
-   int turnCount= 0;
-   while (gameState == ONGOING) {
+   int turnCount = 0;
+   while (gameState == ONGOING)
+   {
       turnCount++;
       std::cout << "\033[Fturn " << turnCount << "\n";
       string FEN = game.getFEN();
@@ -285,9 +334,12 @@ void Abbie::trainOneGame() {
 
    float starting_eval = 0.5f;
    float ending_eval;
-   if (gameState == CHECKMATE) {
+   if (gameState == CHECKMATE)
+   {
       ending_eval = (whitePlaying ? 1.f : 0.f);
-   } else {
+   }
+   else
+   {
       // draw
       ending_eval = 0.5f;
    }
@@ -296,7 +348,8 @@ void Abbie::trainOneGame() {
    vector<std::shared_ptr<std::mutex>> b_mtx;
    vector<Mat> weightGrads = model_.weightsZero();
    vector<Mat> biasGrads = model_.biasesZero();
-   for (unsigned i = 0; i < weightGrads.size(); i++) {
+   for (unsigned i = 0; i < weightGrads.size(); i++)
+   {
       w_mtx.push_back(make_shared<std::mutex>());
       b_mtx.push_back(make_shared<std::mutex>());
    }
@@ -304,14 +357,16 @@ void Abbie::trainOneGame() {
    std::cout << "Game was " << FENs.size() - 1 << " moves long\n";
    std::cout << "Computing weight and bias gradients for state 0\n";
 
-   vector<future<void>> activePool {};
-   for (unsigned state = 0; state < FENs.size(); state++) {
+   vector<future<void>> activePool{};
+   for (unsigned state = 0; state < FENs.size(); state++)
+   {
       shrinkActivePoolToSize(activePool, 6);
       activePool.push_back(std::async(compute_W_B_forState, &model_, &FENs, state, starting_eval, ending_eval, &weightGrads, &w_mtx, &biasGrads, &b_mtx));
       std::cout << "\033[F\33[2KComputing weight and bias gradients for state " << state << std::endl;
    }
 
-   for (auto& ftr: activePool) {
+   for (auto &ftr : activePool)
+   {
       ftr.wait();
    }
 
@@ -319,8 +374,8 @@ void Abbie::trainOneGame() {
    model_.adjustWeightsAndBiases(weightGrads, biasGrads, 0.03, FENs.size());
 }
 
-
-void Abbie::trainOneBoard() {
+void Abbie::trainOneBoard()
+{
    Chess game;
    float eval;
 
@@ -339,7 +394,8 @@ void Abbie::trainOneBoard() {
    vector<Mat> biasGrads = model_.biasesZero();
 
    unsigned board = 0;
-   for(auto FEN: {FEN1, FEN2}) {
+   for (auto FEN : {FEN1, FEN2})
+   {
       board++;
       float eval_shouldBe = 0.5;
 
@@ -353,7 +409,8 @@ void Abbie::trainOneBoard() {
       std::cout << "computing weight gradients\n";
       auto nw = model_.computeWeights(diffs, as);
       std::cout << "adding weight gradients to gradient sum\n";
-      for (int w = 0; w < weightGrads.size(); w++) {
+      for (int w = 0; w < weightGrads.size(); w++)
+      {
          auto newWeightGrad = weightGrads[w] + nw[w];
          weightGrads[w] = newWeightGrad;
       }
@@ -361,12 +418,12 @@ void Abbie::trainOneBoard() {
       std::cout << "computing bias gradients\n";
       auto nb = model_.computeBiases(diffs);
       std::cout << "adding bias gradients to gradient sum\n";
-      for (int b = 0; b < biasGrads.size(); b++) {
+      for (int b = 0; b < biasGrads.size(); b++)
+      {
          auto newBiasGrad = biasGrads[b] + nb[b];
          biasGrads[b] = newBiasGrad;
       }
    }
-
 
    std::cout << "Adjusting weights and biases\n";
    model_.adjustWeightsAndBiases(weightGrads, biasGrads, 0.003, 2);
